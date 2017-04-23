619a0748dbee8b9322f2b5fef34bdecc8e0bc33f
ztimofeev <ztimofeev01@gmail.com>
2017-04-23 21:47:09 +0300

    Added new solves of tasks and finished the StuckZipper task

---
+++ NoteStatistics/NoteStatistics.cs
@@ -0,0 +1,55 @@
+﻿namespace NoteStatistics
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class NoteStatistics
+    {
+        public static void Main()
+        {
+            List<string> notes = new List<string>() { "C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B" };
+            List<double> frequences = new List<double>() { 261.63, 277.18, 293.66, 311.13, 329.63, 349.23, 369.99, 392.00, 415.30, 440.00, 466.16, 493.88 };
+
+            var input = Console.ReadLine().Split().Select(double.Parse).ToList();
+
+            List<string> naturalNotes = new List<string>();
+            List<string> sharpedNotes = new List<string>();
+            List<string> foundNotes = new List<string>();
+
+            double naturalSum = 0;
+            double sharpedSum = 0;
+
+            for (int i = 0; i < input.Count; i++)
+            {
+                for (int j = 0; j < frequences.Count; j++)
+                {
+                    if (input[i] == frequences[j])
+                    {
+                        foundNotes.Add(notes[j]);
+
+                        foreach (var item in notes[j])
+                        {
+                            if (item != '#' && notes[j].Length == 1)
+                            {
+                                naturalNotes.Add(notes[j]);
+                                naturalSum += frequences[j];
+                            }
+                            else if (item == '#' && notes[j].Length == 2)
+                            {
+                                sharpedNotes.Add(notes[j]);
+                                sharpedSum += frequences[j];
+                            }
+                        }
+                    }
+                }
+            }
+
+            Console.WriteLine("Notes: " + string.Join(" ", foundNotes));
+            Console.WriteLine("Naturals: " + string.Join(", ", naturalNotes));
+            Console.WriteLine("Sharps: " + string.Join(", ", sharpedNotes));
+            Console.WriteLine($"Naturals sum: {naturalSum}");
+            Console.WriteLine($"Sharps sum: { sharpedSum}");
+        }
+    }
+}
---
+++ StuckZipper/StuckZipper.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StuckZipper
+{
+    public class StuckZipper
+    {
+        public static void Main()
+        {
+            var list1 = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            var list2 = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+
+            var minNumberOfDigits = CalculateNumberOfDigits(list1, list2);
+
+            RemovingNumbersWithMoreDigits(list1, minNumberOfDigits);
+            RemovingNumbersWithMoreDigits(list2, minNumberOfDigits);
+
+            var insertionIndex = 1;
+            for (int i = 0; i < list1.Count; i++)
+            {
+                list2.Insert(Math.Min(insertionIndex, list2.Count), list1[i]);
+                insertionIndex += 2;
+            }
+
+            Console.WriteLine(string.Join(" ", list2));
+        }
+
+        public static List<int> RemovingNumbersWithMoreDigits(List<int> list, int bestNumberOfDigits)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                var currentNumberOfDigits = CoutNumberOfDigits(list[i]);
+
+                if (currentNumberOfDigits > bestNumberOfDigits)
+                {
+                    list.RemoveAt(i);
+                    i--;
+                }
+            }
+
+            return list;
+        }
+
+        public static int CalculateNumberOfDigits(List<int> list1, List<int> list2)
+        {
+            var minNumberOfDigits = int.MaxValue;
+
+            foreach (var number in list1)
+            {
+                var numberOfDigits = CoutNumberOfDigits(number);
+                if (numberOfDigits < minNumberOfDigits)
+                {
+                    minNumberOfDigits = numberOfDigits;
+                }
+            }
+
+            foreach (var number in list2)
+            {
+                var numberOfDigits = CoutNumberOfDigits(number);
+                if (numberOfDigits < minNumberOfDigits)
+                {
+                    minNumberOfDigits = numberOfDigits;
+                }
+            }
+
+            return minNumberOfDigits;
+        }
+
+        public static int CoutNumberOfDigits(int number)
+        {
+            number = Math.Abs(number);
+            var numberOfDigits = number.ToString().Length;
+
+            return numberOfDigits;
+        }
+    }
+}
